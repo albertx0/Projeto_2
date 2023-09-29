5cf405a7e8ec1d7adcb96db987894fcb566b8bab
unifgpinheiro <pinheiro.gabriel2020@gmail.com>
2023-09-29 17:42:20 -0300

    Arquivos.

---
+++ biblioteca.h
@@ -0,0 +1,24 @@
+//
+// Created by unifgpinheiro on 29/09/2023.
+//
+
+#ifndef PROJETO_2_BIBLIOTECA_H
+#define PROJETO_2_BIBLIOTECA_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct{
+    char nome[100];
+    char CPF[15];
+    char Tipo_conta[10];
+    double Saldo;
+    char Senha[50];
+} Dados;
+
+typedef struct{
+    Dados lista[1000];
+    int qtd;
+} Clientes;
+
+#endif //PROJETO_2_BIBLIOTECA_H
