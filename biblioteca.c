3966bb4439470bd1f718ae45715ce47ff2ec6b1a
PauloGabrielLeme <132236197+PauloGabrielLeme@users.noreply.github.com>
2023-10-18 23:29:02 -0300

    Nova Lógica da Função de cadastro

--- biblioteca.c
+++ biblioteca.c
@@ -1,7 +1,39 @@
 #include "biblioteca.h"
 
 void cadastrar(char *nome, double saldo, char *CPF, char *tc, char *senha){
-   
+    Dados pessoa;
+
+    // Verifica se as strings não excedem os limites das strings
+    if (strlen(nome) >= 100 || strlen(CPF) >= 15 || strlen(tc) >= 10 || strlen(senha) >= 50) {
+        printf("Erro: uma ou mais strings ultrapassaram os limites de tamanho.\n");
+        return;
+    }
+    //Copia as variaveis do struct para as variaveis da função
+    strcpy(pessoa.nome, nome);
+    strcpy(pessoa.CPF, CPF);
+    strcpy(pessoa.Tipo_conta, tc);
+    pessoa.Saldo = saldo;
+    strcpy(pessoa.Senha, senha);
+
+    // Abrindo o arquivo para escrita binária
+    FILE *arquivo = fopen("dados.bin", "ab");
+
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        printf("Erro Ao Abrir o Arquivo\n");
+    }
+
+    // Escrevendo a estrutura no arquivo
+    size_t elementos_gravados = fwrite(&pessoa, sizeof(Dados), 1, arquivo);
+
+    if (elementos_gravados != 1) {
+        printf("Erro ao escrever no arquivo.\n");
+        fclose(arquivo);
+        // Termina a execução em caso de erro
+    }
+
+    // Fechando o arquivo
+    fclose(arquivo);
   
   
 }
--- main.c
+++ main.c
@@ -1,14 +1,7 @@
 #include "biblioteca.h"
 
 int main(){
-    //Variáveis de teste
-  char *n = "Paulo";
-  char *c = "528.303.128.48";
-  char *t = "Comum";
-  char *s = "Teste";
-  double saldo = 2000;
-  //Variáveis de teste
-  criar_cliente(n,c,t,saldo,s);
+    
 
     return 0;
 }
